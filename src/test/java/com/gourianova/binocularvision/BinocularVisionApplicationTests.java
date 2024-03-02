c50e6a90c1aa202df720f89ca27c9a0f2e28aba8
Татьяна Гурьянова <62667085+Tatiana-Gourianova@users.noreply.github.com>
2024-03-02 19:36:17 +0300

    Add files via upload

---
+++ src/test/java/com/gourianova/binocularvision/BinocularVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gourianova.binocularvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinocularVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
